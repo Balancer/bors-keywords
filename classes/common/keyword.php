4be8ef2962fd78ac5e38d7167adcda9e7198937d
Balancer <balancer@balancer.ru>
2010-07-26 17:48:32 +0400

    ++

--- classes/common/keyword.php
+++ classes/common/keyword.php
@@ -24,7 +24,7 @@ class common_keyword extends base_page_db
 	static function normalize($words)
 	{
 		$keywords = array();
-		$Stemmer = &new Lingua_Stem_Ru();
+		$Stemmer = new Lingua_Stem_Ru();
 
 		foreach(explode(' ', bors_lower($words)) as $word)
 			if($word)
@@ -55,6 +55,7 @@ class common_keyword extends base_page_db
 
 	static function keyword_search_reindex($kw)
 	{
+		$count = 0;
 		require_once('inc/search/sphinx.php');
 
 		$xs = bors_search_sphinx($kw, array(
@@ -67,19 +68,38 @@ class common_keyword extends base_page_db
 			'filter' => array('forum_id<>' => array(37)),
 		));
 
+//		print_r($xs);
+
 		if(!is_array($xs))
-			return;
+			return 0;
+
+		$ucase = ($kw == bors_upper($kw)); // тэг в верхнем регистре. Сокращение/аббревиатура.
 
 		foreach($xs as $x)
 		{
 			if(in_array($kw, $x->keywords()))
 				continue;
 
-			$x->add_keyword($kw, true);
-			common_keyword_bind::add($x);
+			if($ucase && strpos($x->title(), $kw) === false && strpos($x->description(), $kw) === false)
+			{
+//				echo "Remove tag $kw from {$x->debug_title()}, kw={$x->keywords_string()}\n";
+//				$x->remove_keyword($kw, true);
+//				common_keyword_bind::add($x);
+				continue;
+			}
+
+			if(stripos($x->title(), $kw) !== false || stripos($x->description(), $kw) !== false)
+			{
+				echo "Add tag $kw to {$x->debug_title()}, kw={$x->keywords_string()}\n";
+				$x->add_keyword($kw, true);
+				common_keyword_bind::add($x);
+				$count++;
+				continue;
+			}
 		}
 
 		bors()->changed_save();
+		return $count;
 	}
 
 	static function best_forum($keywords_string)
@@ -139,23 +159,44 @@ class common_keyword extends base_page_db
 			foreach($kwbs as $kwb)
 			{
 				$topic = object_load('balancer_board_topic', $kwb->target_container_object_id());
-				echo "$tag [{$kwb->total()}, ".round(sqrt($kwb->total()), 1).", ".round(log($kwb->total())+1, 1)."]: {$topic->debug_title()}\n";
-				@$ids[$kwb->target_container_object_id()] += log($kwb->total())+1;
+//				echo "$tag [{$kwb->total()}, ".round(sqrt($kwb->total()), 1).", ".round(log($kwb->total())+1, 1)."]: {$topic->debug_title()}\n";
+				@$ids[$kwb->target_container_object_id()] += sqrt($kwb->total());
 			}
 		}
 
 		asort($ids);
-
+/*
 		echo "\n=== sorted result: ===\n";
 		foreach($ids as $id => $count)
 		{
 			$topic = object_load('balancer_board_topic', $id);
 			echo "{$topic->debug_title()}: $count\n";
 		}
-
+*/
 		if($ids)
 			$topic_id = array_pop(array_keys($ids));
 
 		return $topic_id;
+	}
+
+	static function compare_eq($kws1, $kws2) { return self::normalize($kws1) == self::normalize($kws2); }
+
+	function change_synonym()
+	{
+		if(!$this->synonym_to_id())
+			return 0;
+
+		$syn = object_load('common_keyword', $this->synonym_to_id());
+		foreach(objects_array('common_keyword_bind', array('keyword_id' => $this->id())) as $bind)
+		{
+			$obj = $bind->target();
+			echo "{$obj->debug_title()}: change {$this->title()} to {$syn->title()}\n";
+			$obj->remove_keyword($this->title(), true);
+			$obj->add_keyword($syn->title(), true);
+		}
+
+		bors()->changed_save();
+		echo $this->set_targets_count(objects_count('common_keyword_bind', array('keyword_id' => $this->id())), true);
+		echo $syn->set_targets_count(objects_count('common_keyword_bind', array('keyword_id' => $syn->id())), true);
 	}
 }
