2a12e7e959e8e1fe64bfb1a9695a44bd34a450c4
Balancer <balancer@balancer.ru>
2011-05-30 00:30:45 +0400

    Всяко разно.

---
+++ classes/bors/admin/tags/main.php
@@ -0,0 +1,18 @@
+<?php
+
+class bors_admin_tags_main extends bors_admin_paginated
+{
+	function title() { return ec('Редактор тэгов'); }
+
+	function main_class() { return 'common_keyword'; }
+	function order() { return '-modify_time'; }
+
+	function where()
+	{
+		return array_merge(parent::where(), array(
+			'(target_containers_count<> 0 OR targets_count<>0)'
+		));
+	}
+
+	function items_per_page() { return 50; }
+}
